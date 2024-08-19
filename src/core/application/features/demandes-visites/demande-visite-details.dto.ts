d46110e41a866b24e92337fec13579a73ddf7e53
Karamoko Israel <karamokoisrael@gmail.com>
2024-08-19 13:27:23 +0800

    fix(demandes-visites): cannot get demande visite by id

---
+++ src/core/application/features/demandes-visites/demande-visite-details.dto.ts
@@ -0,0 +1,78 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { OmitMethods } from "@/lib/ts-utilities";
+import { WrapperResponseDto } from "@/lib/responses";
+import { BienImmobilierDto } from "@/core/application/features/biens-immobiliers";
+import { BienImmobilier } from "@/core/domain/biens-immobiliers";
+import { StatusDemandeVisite, TypeDemandeVisite } from "@/core/domain/demandes-visites";
+import { ServiceDateDto } from "@/core/application/shared/dto";
+import { ServiceDates } from "@/core/domain/shared/models";
+import { StatusFacture } from "@/core/domain/payments";
+import { PublicUserInfoDto } from "@/core/application/features/users";
+import { PublicUserInfo } from "@/core/domain/users";
+
+export class DemandeVisiteDetailsDto {
+
+  @ApiProperty({ format: "uuid" })
+  id: string;
+
+  @ApiProperty({ enum: StatusDemandeVisite })
+  statusDemandeVisite: StatusDemandeVisite;
+
+  @ApiProperty({ enum: TypeDemandeVisite })
+  typeDemandeVisite: TypeDemandeVisite;
+
+  @ApiProperty({ type: ServiceDateDto, isArray: true })
+  datesDemandeVisite: ServiceDates;
+
+  @ApiProperty({ enum: StatusFacture })
+  statusFacture: StatusFacture;
+
+  @ApiProperty()
+  retraitProEffectue: boolean;
+
+  @ApiProperty()
+  montantTotalDemandeVisite: number;
+
+  @ApiProperty()
+  montantDemandeVisiteSansCommission: number;
+
+  @ApiProperty()
+  notes: string;
+
+  @ApiProperty()
+  clientPhoneNumber: string;
+
+  @ApiProperty({ type: () => BienImmobilierDto })
+  bienImmobilier: BienImmobilier;
+  @ApiProperty({ format: "uuid" })
+  bienImmobilierId: string;
+
+  @ApiProperty({ type: () => PublicUserInfoDto })
+  client: PublicUserInfo;
+  @ApiProperty({ type: () => PublicUserInfoDto })
+  proprietaire: PublicUserInfo;
+
+  @ApiProperty()
+  createdAt?: Date;
+
+  @ApiProperty()
+  updatedAt?: Date;
+
+  @ApiProperty()
+  deletedAt?: Date;
+
+  @ApiProperty()
+  createdBy?: string;
+
+  constructor(data?: OmitMethods<DemandeVisiteDetailsDto>) {
+    if (data) Object.assign(this, data);
+  }
+}
+
+export class WrapperResponseDemandeVisiteDetailsDto extends WrapperResponseDto<DemandeVisiteDetailsDto> {
+  @ApiProperty({ type: DemandeVisiteDetailsDto })
+  data: DemandeVisiteDetailsDto;
+}
+
+
+
