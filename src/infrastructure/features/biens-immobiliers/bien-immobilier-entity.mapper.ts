ef1b4b21c7f1fa268b239dff7f2a0cf17f5336e2
Israel <karamokoisrael@gmail.com>
2024-10-02 17:12:29 +0800

    fix(api): solving mapping bugs

--- src/infrastructure/features/biens-immobiliers/bien-immobilier-entity.mapper.ts
+++ src/infrastructure/features/biens-immobiliers/bien-immobilier-entity.mapper.ts
@@ -6,35 +6,11 @@ import { getIdFromObject } from "@/lib/ts-utilities/mapping";
 export class BienImmobilierEntityMapper implements IMapper<BienImmobilierEntity, BienImmobilier> {
   mapFrom(object: OmitMethods<BienImmobilierEntity>): BienImmobilier {
     return new BienImmobilier({
-      id: object.id,
+      ...object,
       miniature: getIdFromObject(object.miniature),
-      nom: object.nom,
-      typeBienImmobilier: object.typeBienImmobilier,
-      description: object.description,
-      typeLocation: object.typeLocation,
-      aLouer: object.aLouer,
-      amentities: object.amentities,
-      tags: object.tags,
-      images: object.images,
       video: getIdFromObject(object.video),
       ville: getIdFromObject(object.ville),
       commune: getIdFromObject(object.commune),
-      adresse: object.adresse,
-      position: object.position,
-      statusValidation: object.statusValidation,
-      prix: object.prix,
-      metadata: object.metadata,
-      featured: object.featured,
-      nombreMaxOccupants: object.nombreMaxOccupants,
-      animauxAutorises: object.animauxAutorises,
-      bienImmobilierDisponible: object.bienImmobilierDisponible,
-      fetesAutorises: object.fetesAutorises,
-      reglesSupplementaires: object.reglesSupplementaires,
-      proprietaire: object.proprietaire,
-      createdAt: object.createdAt,
-      updatedAt: object.updatedAt,
-      deletedAt: object.deletedAt,
-      createdBy: object.createdBy,
     });
   }
 
