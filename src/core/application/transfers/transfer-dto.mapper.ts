f1fa2e2679eac7f886b257994bda27117a2b64d6
John <lamagniguitoure@gmail.com>
2026-02-28 08:25:15 +0000

    fix(transfer): customer data

--- src/core/application/transfers/transfer-dto.mapper.ts
+++ src/core/application/transfers/transfer-dto.mapper.ts
@@ -1,17 +1,15 @@
 import { Transfer } from "@/core/domain/transfers/transfer.model";
 import { IMapper, OmitMethods } from "@/lib/ts-utilities";
 import { TransferDto } from "./transfer.dto";
-import { getIdFromObject } from "@/lib/ts-utilities/mapping";
 
 export class TransferDtoMapper implements IMapper<Transfer, TransferDto> {
   mapFrom(param: OmitMethods<Transfer>): TransferDto {
     return new TransferDto({
       ...param,
-      customer: getIdFromObject(param.customer),
     });
   }
 
-  mapTo(param: OmitMethods<TransferDto>): Transfer {
+  mapTo(param: Partial<TransferDto>): Transfer {
     return new Transfer(param);
   }
 }
